6a3357abcc565a1753510df3f660025a7113d519
Alex <xander1599@gmail.com>
2019-08-12 09:33:12 -0600

    ConsoleIO Import & Menu added to start new game

---
+++ ChessMovement-Shanes-branch/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessMovement
+{
+    class Menu
+    {
+        private static Util util = new Util(); // Sets up the Util class so we can call our needed methodd
+
+        public static void LoadGame(String[] args)
+        {
+            if (util.ArgCheck(args)) //Checks if an argument is present and if it leads to a valid file
+            {
+                util.setupBoard();
+                util.FileReader(args[0]);
+            }
+        }
+
+        public static void NewGame()
+        {
+            util.setupBoard();
+        }
+
+    }
+}
--- ChessMovement-Shanes-branch/Program.cs
+++ ChessMovement-Shanes-branch/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSC160_ConsoleMenu;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,15 +10,21 @@ namespace ChessMovement
     class Program
     {
         static void Main(string[] args)
-
         {
-            Util util = new Util(); // Sets up the Util class so we can call our needed methodd
-            if (util.ArgCheck(args)) //Checks if an argument is present and if it leads to a valid file
-            {
-                util.setupBoard();
-                util.FileReader(args[0]);
-            }
 
+            String[] options = { "New Game", "Load Game" };
+            int option = CIO.PromptForMenuSelection(options, true);
+            switch (option)
+            {
+                case 1:
+                    Menu.NewGame();
+                    break;
+                case 2:
+                    Menu.LoadGame(args);
+                    break;
+                case 0:
+                    break;
+            }
         }
     }
 }
